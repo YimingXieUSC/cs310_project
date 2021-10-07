6378d398baf62f695963e0d367a3a09421779d70
Yiming Xie <yimingx@usc.edu>
2021-10-07 04:38:02 -0700

    modified the styling

--- components/navbar.js
+++ components/navbar.js
@@ -6,7 +6,7 @@ class NavBar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Your Proposed Group Dates</a>
+    <a class="navbar-brand" href="#"><h2>GroupApp</h2></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
