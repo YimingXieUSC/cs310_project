ac77412b096baba8f07282758dd212234931e39a
YimingXieUSC <yimingx@usc.edu>
2021-10-07 21:23:15 -0700

    with liz's file

--- components/navbar.js
+++ components/navbar.js
@@ -14,7 +14,7 @@ class NavBar extends HTMLElement {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="mainpage.html">Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
